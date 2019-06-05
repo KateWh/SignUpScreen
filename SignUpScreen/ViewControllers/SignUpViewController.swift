2fb9193607323e9f86e74eab6cc658c94edf42e7
Ekaterina Romanchenko <romanchenko.e.e@gmail.com>
2019-06-05 23:10:50 +0300

    Implement signUp screen.

---
+++ SignUpScreen/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,177 @@
+//
+//  ViewController.swift
+//  SignUpScreen
+//
+//  Created by ket on 6/3/19.
+//  Copyright © 2019 ket. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpScrollView: UIScrollView!
+    @IBOutlet weak var scrollBottomConstaint: NSLayoutConstraint!
+    var currentTextField: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingTextFields()
+        signUpScrollView.delegate = self
+        registerKeyboardNotification()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        removeKeyboardNotifications()
+    }
+    
+}
+
+extension SignUpViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.x != 0 {
+            scrollView.contentOffset.x = 0
+        }
+    }
+    
+}
+
+extension SignUpViewController: UITextFieldDelegate {
+    
+    func settingTextFields() {
+        nameTextField.delegate = self
+        nameTextField.tag = 0
+        nameTextField.returnKeyType = UIReturnKeyType.next
+        setPaddingForTextField(nameTextField)
+        usernameTextField.delegate = self
+        usernameTextField.tag = 1
+        usernameTextField.returnKeyType = UIReturnKeyType.next
+        setPaddingForTextField(usernameTextField)
+        emailTextField.delegate = self
+        emailTextField.tag = 2
+        emailTextField.returnKeyType = UIReturnKeyType.next
+        setPaddingForTextField(emailTextField)
+        passwordTextField.delegate = self
+        passwordTextField.tag = 3
+        passwordTextField.returnKeyType = UIReturnKeyType.done
+        setPaddingForTextField(passwordTextField)
+    }
+    
+    func setPaddingForTextField(_ textField: UITextField) {
+        let spacerView = UIView(frame:CGRect(x:0, y:0, width:10, height:10))
+        textField.leftViewMode = UITextField.ViewMode.always
+        textField.leftView = spacerView
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("Внутри textFieldShouldBeginEditing")
+        print("textField.tag", textField.tag)
+        currentTextField = textField
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            return true
+        }
+        return false
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        switch textField.tag {
+        case 0:
+            if textField.text?.count ?? 0 < 4 {
+                showAlert(title: "Name too short!", massage: "Must enter more than 3 characters.", preferredStyle: .alert, titleAction: "Ok", styleAction: .default, handlerAction: nil)
+                return false
+            }
+        case 3:
+            let text = textField.text ?? ""
+            if !checkLength(textField.text, range: (6,16)) || !text.isAlphanumeric  {
+                showAlert(title: "Wrong input!", massage: "The password must contain only letters and numbers, from 6 to 16 characters.", preferredStyle: .alert, titleAction: "Ok", styleAction: .default, handlerAction: nil)
+                return false
+            }
+        default:
+            break
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        currentTextField = nil
+    }
+    
+    func checkLength(_ string: String?, range: (Int,Int)) -> Bool {
+        if let string = string {
+            switch string.count{
+            case range.0...range.1:
+                return true
+            default:
+                break
+            }
+        }
+        return false
+    }
+    
+}
+
+extension SignUpViewController {
+    
+    func showAlert(title: String?, massage: String?, preferredStyle: UIAlertController.Style, titleAction: String?, styleAction: UIAlertAction.Style, handlerAction: ((UIAlertAction) -> Void)?) {
+        let alertController = UIAlertController(title: title, message: massage, preferredStyle: preferredStyle)
+        let alertAction = UIAlertAction(title: titleAction, style: styleAction, handler: handlerAction)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion:  nil)
+    }
+    
+}
+
+extension SignUpViewController {
+    
+    func registerKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:
+            UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(_ notification: Notification) {
+        print("Внутри keyboardWillShow")
+        let userInfo = notification.userInfo
+        let keyboardFrameSize = (userInfo? [UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        print("Current text field", currentTextField?.tag)
+        
+        //scrollBottomConstaint = NSLayoutConstraint(
+          //  CGPoint(x: 0, y: keyboardFrameSize.height)
+        guard let currentTextField = currentTextField else { return }
+        print("CURRENT TEXT FIELD minY",currentTextField.frame.minY)
+        print("CURRENT TEXT FIELD maxY",currentTextField.frame.maxY)
+        
+        print("KEYBOARD FRAME SIZE minY",keyboardFrameSize.minY)
+        print("KEYBOARD FRAME SIZE maxY",keyboardFrameSize.maxY)
+    }
+    
+    @objc func keyboardWillHide() {
+        signUpScrollView.contentOffset = CGPoint.zero
+    }
+    
+}
+
+extension String {
+    
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+    
+}
